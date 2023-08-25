ff64d35afe8b7d4c8eb10b2fff0f5e4c189f117a
Paco <paco.refractando@gmail.com>
2023-08-25 12:43:58 +0200

    comenzado js

---
+++ Cronometro/app.js
@@ -0,0 +1,5 @@
+const botonInicioPausa = document.getElementById('boton-inicio-pausa');
+const botonReiniciar = document.getElementById('boton-reiniciar');
+
+let[hora, minutos, segundos]=[0,0,0];
+let intervaloTiempo;
