28b216fcbb36840c2e5f5936c31a6392ef7063a3
Paco <paco.refractando@gmail.com>
2023-08-27 13:47:08 +0200

    acabado js y anadido footer en css

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -17,21 +17,40 @@ function agregarTarea(){
         let iconos=document.createElement('div');
         iconos.classList.add('iconos');
         tareaNueva.appendChild(iconos);
-
+        //se crean los iconos y se le da un eventlistener
         let completar = document.createElement('i');
         completar.classList.add('bi', 'bi-check-square-fill','icono-completar');
+        completar.addEventListener('click', completarTarea)
 
         let eliminar = document.createElement('i');
         eliminar.classList.add('bi', 'bi-trash3-fill','icono-eliminar');
-        
+        eliminar.addEventListener('click',eliminarTarea);
+        //se anaden los iconos a la jerarquia y la tarea al DOM
         iconos.append(completar, eliminar);
         listaTareas.appendChild(tareaNueva);
+        //limpiamos el input
+        input.value='';
+
 
     }else{
         alert('Debe ingresar una tarea');
     }
 }
 
-boton.addEventListener('click',agregarTarea);
+function completarTarea(e){
+    let tarea=e.target.parentNode.parentNode;
+    tarea.classList.toggle('completada');//si tiene la clase completada, la quita, si no, la anade
+}
 
+function eliminarTarea(e){
+    let tarea = e.target.parentNode.parentNode;
+    tarea.remove();
+}
+
+boton.addEventListener('click',agregarTarea);
+input.addEventListener('keydown',(e)=>{
+    if( e.key==='Enter'){
+        agregarTarea();
+    }
+})
 
